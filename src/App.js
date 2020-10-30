370608386879e4c58a746c753f7b46c86cc12d07
Hovhannes <tonoyan6666@gmail.com>
2020-10-30 17:55:51 +0400

    some bug fix

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import { connect } from "react-redux";
 import { setFinished, setSteps, setRestart } from "./redux/actions";
 
 function App({ finished, setFinished, steps, setSteps, setRestart }) {
-  
   const handleSetFinished = () => {
     setFinished(true);
   };
--- src/index.js
+++ src/index.js
@@ -4,14 +4,12 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
-import thunk from "redux-thunk";
-import { applyMiddleware, compose, createStore } from "redux";
+import { compose, createStore } from "redux";
 import { gameReducers } from "./redux/gameReducers";
 
 const store = createStore(
   gameReducers,
   compose(
-    applyMiddleware(thunk),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
