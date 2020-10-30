0d5e9ebf36815567ebc0aec3cde315542885992f
Hovhannes <tonoyan6666@gmail.com>
2020-10-30 16:54:08 +0400

    style change

--- src/components/WonContainer/index.js
+++ src/components/WonContainer/index.js
@@ -4,8 +4,11 @@ export default function WonContainer({ steps, handlePlayAgain }) {
   return (
     <>
       <h1 className="you-won">You Won ! </h1>
-      <h2>TOTAL STEPS: {steps}</h2>
-      <button onClick={() => handlePlayAgain({ finished: false, steps: 0 })}>
+      <h2 className="you-won">TOTAL STEPS: {steps}</h2>
+      <button
+        className="again"
+        onClick={() => handlePlayAgain({ finished: false, steps: 0 })}
+      >
         Play Again
       </button>
     </>
